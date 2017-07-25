a5ef31140243ce5d5528091efe2c3f34cdbd5645
ReFantasy <309741309@qq.com>
2017-07-25 22:29:03 +0800

    ex1-11

--- ch01/ex1_11.cpp
+++ ch01/ex1_11.cpp
@@ -2,11 +2,12 @@
 
 int main(int argc, char* argv[])
 {
-	int first, second, tmp_first, tmp_second;
+	int first, second;
+	int tmp_first, tmp_second;  //临时变量
 	std::cout << "Enter two numbers: ";
 	std::cin >> first >> second;
-
-    tmp_first = first;
+	
+	tmp_first = first;
 	tmp_second = second;
 	while( tmp_second - tmp_first >= 0)
 	{
