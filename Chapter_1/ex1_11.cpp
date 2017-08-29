a68f98e4723dd633c54ccc2e83e997ac8ea8486b
ReFantasy <309741309@qq.com>
2017-08-29 21:55:26 +0800

    update ex1.11

--- Chapter_1/ex1_11.cpp
+++ Chapter_1/ex1_11.cpp
@@ -6,29 +6,33 @@ int main(int argc, char* argv[])
 	std::cout << "Enter two numbers: ";
 	std::cin >> first >> second;
 	
+	//如果输入的两个参数相等 直接输出并结束程序
         while(second == first)
 	{
 	    std::cout<<first<<std::endl;
 	    return 0;
 	}
 
+	//第二个参数大于第一个参数
 	while( second - first >= 0)
 	{
 		std::cout << first << " ";
 		++first;
 	}
 	
-	std::cout<<std::endl;
-
+		
 	while((second + 1) == first)
+	{
+	    std::cout<<std::endl;
 	    return 0;
-
+	}
+	//第二个参数小于第一个参数
 	while(second - first <= 0)
 	{
 		std::cout << second << " ";
 		++second;
 	}
 	
-	std::cout << std::endl;  //空格
+	std::cout<<std::endl;
 	return 0;
 }
