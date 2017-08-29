09dce477b12f0538477b2030923dad63d697b475
ReFantasy <309741309@qq.com>
2017-08-29 21:45:19 +0800

    update ex1.11

--- Chapter_1/ex1_11.cpp
+++ Chapter_1/ex1_11.cpp
@@ -3,24 +3,30 @@
 int main(int argc, char* argv[])
 {
 	int first, second;
-	int tmp_first, tmp_second;  //临时变量
 	std::cout << "Enter two numbers: ";
 	std::cin >> first >> second;
 	
-	tmp_first = first;
-	tmp_second = second;
-	while( tmp_second - tmp_first >= 0)
+        while(second == first)
 	{
-		std::cout << tmp_first << " ";
-		++tmp_first;
+	    std::cout<<first<<std::endl;
+	    return 0;
 	}
 
-	tmp_first = first;
-	tmp_second = second;
-	while(tmp_first - tmp_second >= 0)
+	while( second - first >= 0)
 	{
-		std::cout << tmp_second << " ";
-		++tmp_second;
+		std::cout << first << " ";
+		++first;
+	}
+	
+	std::cout<<std::endl;
+
+	while((second + 1) == first)
+	    return 0;
+
+	while(second - first <= 0)
+	{
+		std::cout << second << " ";
+		++second;
 	}
 	
 	std::cout << std::endl;  //空格
