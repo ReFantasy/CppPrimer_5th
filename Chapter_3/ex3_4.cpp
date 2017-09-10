cc016b5cfe2a043181f02e452e7651907bdb14e9
ReFantasy <309741309@qq.com>
2017-09-10 19:27:21 +0800

    3.4

---
+++ Chapter_3/ex3_4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string s1,s2;
+    cin >> s1 >> s2;
+
+    if(s1 > s2)
+	cout << s1 << endl;
+    else if(s2 > s1)
+	cout << s2 <<endl;
+
+    return 0;
+}
+
