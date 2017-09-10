03d537ce3d247ed310dfd5bf26272d54cd9feffd
ReFantasy <309741309@qq.com>
2017-09-10 19:31:41 +0800

    3.5

---
+++ Chapter_3/ex3_5.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string sum,tmp;
+
+    while(cin >> tmp)
+	sum += tmp;
+
+    cout<<sum<<endl;
+
+    return 0;
+}
