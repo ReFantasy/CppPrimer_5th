7e888af293a38606ac6304dd42bf173f3d407344
ReFantasy <309741309@qq.com>
2017-09-27 20:09:12 +0800

    shell commit

---
+++ Chapter_6/6_25.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+int main(int argc, char *argv[])
+{
+    if(argc < 3)
+	return 0;
+    std::string s(argv[1]);
+
+    s += argv[2];
+
+    std::cout<<s<<std::endl;
+
+    return 0;
+}
