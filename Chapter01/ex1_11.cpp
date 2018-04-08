a4daaa1b4e566a5c9723ad74c10bf2815aee6581
ReFantasy <309741309@qq.com>
2018-04-08 12:31:54 +0800

    Update ex1_11.cpp

--- Chapter01/ex1_11.cpp
+++ Chapter01/ex1_11.cpp
@@ -1,38 +1,32 @@
 #include <iostream>
+using namespace std;
+
+void PrintRange(int v1, int v2)
+{
+	if (v1 <= v2)
+	{
+		while (v1 <= v2)
+		{
+			cout << v1++ << " ";
+		}
+	}
+	else
+	{
+		while (v2 <= v1)
+		{
+			cout << v1-- << " ";
+		}
+	}
+}
 
 int main(int argc, char* argv[])
 {
 	int first, second;
 	std::cout << "Enter two numbers: ";
 	std::cin >> first >> second;
-	
-	//如果输入的两个参数相等 直接输出并结束程序
-        while(second == first)
-	{
-	    std::cout<<first<<std::endl;
-	    return 0;
-	}
 
-	//第二个参数大于第一个参数
-	while( second - first >= 0)
-	{
-		std::cout << first << " ";
-		++first;
-	}
-	
-		
-	while((second + 1) == first)
-	{
-	    std::cout<<std::endl;
-	    return 0;
-	}
-	//第二个参数小于第一个参数
-	while(second - first <= 0)
-	{
-		std::cout << second << " ";
-		++second;
-	}
-	
-	std::cout<<std::endl;
+	PrintRange(first, second);
+	cout << endl;
+
 	return 0;
 }
